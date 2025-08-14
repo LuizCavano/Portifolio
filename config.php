4d275618505fe7e84df3cf395e12c944da36f173
Luiz Cavano Projetos <135461165+LuizCavano@users.noreply.github.com>
2025-08-14 10:52:53 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php (NÃO versionar no GitHub)
+return [
+    'destinatario' => 'seuemail@fatec.sp.gov.br',
+    'smtp_host' => 'smtp.fatec.sp.gov.br',
+    'smtp_usuario' => 'seuemail@fatec.sp.gov.br',
+    'smtp_senha' => 'sua_senha_segura',
+];
