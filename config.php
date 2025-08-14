1a4fb50c7e507df2225c0c8026c1c8ec6b4de8c3
Luiz Cavano Projetos <135461165+LuizCavano@users.noreply.github.com>
2025-08-14 10:53:41 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config.php (NÃO versionar no GitHub)
-return [
-    'destinatario' => 'seuemail@fatec.sp.gov.br',
-    'smtp_host' => 'smtp.fatec.sp.gov.br',
-    'smtp_usuario' => 'seuemail@fatec.sp.gov.br',
-    'smtp_senha' => 'sua_senha_segura',
-];
